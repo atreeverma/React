8c045de18b4c5de50db67c2e2a0be4e155b8831f
atreeverma <atreeverma17@gmail.com>
2025-09-12 19:15:08 +0530

    comm

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,9 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let [counter,setCounter]=useState(15)
+  //let counter=5;
+  const addValue=()=>{
+      //console.log(counter);
+      counter=counter+1//UI updation
+      setCounter(counter)
+  }
+  const removevalue=()=>{
+    //counter--
+    setCounter(counter-1)
+  }
   return (
     <>
       <h1>chai aur react  </h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br/>
+      <button onClick={removevalue}>remove Value</button>
     </>
   )
 }
